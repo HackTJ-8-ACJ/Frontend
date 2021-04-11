edaa3d9d8834b4b3cdf62771156f4afc19984086
CyrilSharma <44011901+CyrilSharma@users.noreply.github.com>
2021-04-11 12:10:11 -0400

    Added some randomized parking

--- app/src/components/parkingLot.tsx
+++ app/src/components/parkingLot.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Container, Row } from 'react-bootstrap';
 import { parking } from './datatypes'
 import { ParkingBlock } from './parking'
@@ -9,10 +9,24 @@ interface I {
 }
 
 export const ParkingLot:React.FC<I> = (props) => {
+  const [data, setData] = React.useState(props.data);
+
+  useEffect(() => {
+      const interval = setInterval(() => { 
+        var new_data = [...data]
+        new_data[Math.floor(Math.random() * new_data.length)].occupied = true;
+        new_data[Math.floor(Math.random() * new_data.length)].occupied = false;
+        setData(new_data)
+      //code goes here that will be run every 5 seconds.    
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [data]);
+
   return <Container>
     <Row className="justify-content-md-center pt-3">
       <svg width={"100%"} height={450}>
-      {props.data.map((value: parking, index: number) => {
+      {data.map((value: parking, index: number) => {
       return <ParkingBlock data={value} size={75} key={index}></ParkingBlock>
       })}
       </svg>
