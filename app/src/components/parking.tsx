35d18cb525929a638f44ed9bf261c03d7c9ee401
CyrilSharma <44011901+CyrilSharma@users.noreply.github.com>
2021-04-10 21:04:31 -0400

    Fixes

--- app/src/components/parking.tsx
+++ app/src/components/parking.tsx
@@ -1,8 +1,6 @@
 export function ParkingBlock(props: any) {
     console.log('Hello!!')
     return (
-    <svg width="31" height="31">
-        <rect x={props.data.point[0]} y={props.data.point[1]} width={20} height={20} transform={`rotate(${props.data.direction})`} stroke="green" strokeWidth={200} fill="purple"/>
-    </svg>
+    <rect x={props.data.point[0]} y={props.data.point[1]} width={20} height={20} transform={`rotate(${props.data.direction})`} stroke="green" strokeWidth={200} fill="purple"/>
     )
 }
--- app/src/components/parkingLot.tsx
+++ app/src/components/parkingLot.tsx
@@ -3,7 +3,7 @@ import { ParkingBlock } from './parking'
 
 export function ParkingLot(props: any) {
 
-  return <svg width="100" height="100">
+  return <svg width="500" height="500">
     {props.data.map((value: parking, index: number) => {
       return <ParkingBlock data={value} key={index}></ParkingBlock>
     })}
