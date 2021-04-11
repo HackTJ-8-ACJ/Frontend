61b6dc42c2e7af526eb543a1d01b177be30ef63c
CyrilSharma <44011901+CyrilSharma@users.noreply.github.com>
2021-04-11 11:04:06 -0400

    Added Toggle Button (unimplemented)

--- app/src/components/parkingLot.tsx
+++ app/src/components/parkingLot.tsx
@@ -11,16 +11,14 @@ interface I {
 export const ParkingLot:React.FC<I> = (props) => {
   return <Container>
     <Row className="justify-content-md-center pt-3">
-      <Col  xs lg="3">
-        <svg width={750} height={450}>
-        {props.data.map((value: parking, index: number) => {
-        return <ParkingBlock data={value} size={75} key={index}></ParkingBlock>
-        })}
-        </svg>
-      </Col>
-      <Col  xs lg="3">
-        <Button variant="dark"> Hello </Button>
-      </Col>
+      <svg width={750} height={450}>
+      {props.data.map((value: parking, index: number) => {
+      return <ParkingBlock data={value} size={75} key={index}></ParkingBlock>
+      })}
+      </svg>
+      </Row>
+    <Row className="justify-content-md-center pt-3">
+      <Button variant="dark"> Toggle Real Data </Button>
     </Row>
   </Container>
 }
